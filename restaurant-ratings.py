4a32486c8724b37f025ecf47e08bb2c2eca2e7ec
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:59:59 -0700

    Completed further study

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,5 +1,6 @@
 # your code goes here
 from sys import argv
+import random
 #can also do import sys
 
 def display_restaurant_ratings(filename):
@@ -35,5 +36,47 @@ def display_restaurant_ratings(filename):
         for restaurant_name, restaurant_rating in sorted(rest_and_ratings.items()):
             print "{} is rated at {}".format(restaurant_name, restaurant_rating)
 
-display_restaurant_ratings(argv[1])
+#display_restaurant_ratings(argv[1])
 #if import sys: (sys.argv[1])
+
+
+def update_random_rests(filename):
+    with open(filename) as restaurant_list: #opens file and sets it equal to the variable restaurant_list
+        rest_and_ratings = {} #creates blank dictionary to store restaurants and ratings
+        for line in restaurant_list: 
+            line = line.rstrip() #strips line of white space
+            keys_values = line.split(':') #splits line into a list with : as a delimiter
+            restaurant_name = keys_values[0] #sets name equal to first item in list
+            restaurant_rating = int(keys_values[1]) #sets rating equal to second item in list, made into an int
+            rest_and_ratings[restaurant_name] = rest_and_ratings.get(restaurant_name, 
+                                                                    restaurant_rating)
+
+    user_name = raw_input("Hi, welcome to our restaurant rating system. What's your name? ")
+
+    list_of_rated_restaurants = []
+
+    user_playing = True
+
+    while user_playing:
+
+        random_restaurant = random.choice(rest_and_ratings.keys())
+        if random_restaurant in list_of_rated_restaurants:
+            random_restaurant = random.choice(rest_and_ratings.keys()) #generate new random restaurant
+        else:
+            list_of_rated_restaurants.append(random_restaurant)
+        
+        current_rating = raw_input("Our current rating for {} is {}. Would you like to re-rate, or quit? If quit, type 'q': ".format(random_restaurant, rest_and_ratings[random_restaurant]))
+        if current_rating == 'q':
+            user_playing = False
+            print sorted(rest_and_ratings.items())
+
+        else:
+            user_rating = raw_input("What do you think this restaurant should be rated {}, on a scale of 1 -5? ".format(user_name))
+            
+            rest_and_ratings[random_restaurant] = user_rating
+
+        
+
+update_random_rests(argv[1])
+
+
